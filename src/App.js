a73cbddeae9e5ae6745035ea2b8695dea468708b
Alice Abarca <aliceabarca@otmail.com>
2023-11-01 13:59:08 -0600

    Add the search news component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import Header from './Components/Header/Header';
 import fetchNews from './api';
 import AllNews from './Components/AllNews/AllNews';
+import SearchNews from './Components/SearchNews/SearchNews';
 
 function App() {
   const [news, setNews] = useState([])
+  const [search, setSearch] = useState('')
   // console.log(news[1].source.id)
   useEffect(() => {
     fetchNews()
@@ -18,6 +20,7 @@ function App() {
     <div className="App">
       <Header />
       <AllNews news={news} />
+      <SearchNews setSearch={setSearch} />
     </div>
   );
 }

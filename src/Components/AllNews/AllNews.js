7450717cea06355c46d097b31ee590c9c32a5d1f
Alice Abarca <aliceabarca@otmail.com>
2023-11-01 10:33:13 -0600

    Add map iterator to go through all key values

--- src/Components/AllNews/AllNews.js
+++ src/Components/AllNews/AllNews.js
@@ -1,10 +1,28 @@
-function AllNews() {
+import News from "../News/News";
 
-  
+
+function AllNews({ news }) {
+
+  // console.log('map', news)
+  const articles = news.map(news => {
+    
+    return (
+     <News 
+      source={news.source.id}
+      author={news.author}
+      content={news.content}
+      description={news.description}
+      publishedAt={news.publishedAt}
+      title={news.title}
+      url={news.url}
+      urlToImage={news.urlToImage}
+    />
+    )
+  })
 
   return (
     <div className="all-news">
-      News
+      {articles}
     </div>
   )
 }

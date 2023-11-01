a2944c82c7b2b559d0463f53acc0bcfe648dd8aa
Alice Abarca <aliceabarca@otmail.com>
2023-10-31 21:43:03 -0600

    Add use effect to app to pass api with all the news article

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
+import fetchNews from './api';
 
 function App() {
+  const [news, setNews] = useState([])
+  
+  useEffect(() => {
+    fetchNews()
+      .then(data => {
+        setNews(data.articles)
+      })
+  }, [])
+
   return (
     <div className="App">
       <Header />

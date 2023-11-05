ae4c157466d8bc2da5302df6c54dfcb500f230e5
Alice Abarca <aliceabarca@otmail.com>
2023-11-05 10:36:54 -0700

    Add another parameter for index

--- src/Components/AllNews/AllNews.js
+++ src/Components/AllNews/AllNews.js
@@ -6,20 +6,23 @@ import { Link } from 'react-router-dom'
 function AllNews({ news, search }) {
   const articles = news
   .filter(news => news.title.toLowerCase().includes(search.toLowerCase()))
-  .map(news => {
+  .map((news, index) => {
     return (
-    <Link to={`/${news.source.id}`} key={news.source.id}>
-      <News 
-        source={news.source.id}
-        author={news.author}
-        content={news.content}
-        description={news.description}
-        publishedAt={news.publishedAt}
-        title={news.title}
-        url={news.url}
-        urlToImage={news.urlToImage}
-      />
-    </Link>
+      <div key={index}>
+      <Link 
+      to={`/article/${index}`}>
+        <News 
+          source={news.source.id}
+          author={news.author}
+          content={news.content}
+          description={news.description}
+          publishedAt={news.publishedAt}
+          title={news.title}
+          url={news.url}
+          urlToImage={news.urlToImage}
+        />
+        </Link>
+    </div>
     )
   })
 
